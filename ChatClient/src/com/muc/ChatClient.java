1af2d7dec0efdfcea8fed850ae260fd87861fb78
developer-ishan <ishan@mnnit.ac.in>
2020-12-09 23:46:09 +0530

    displaying list of online users

--- ChatClient/src/com/muc/ChatClient.java
+++ ChatClient/src/com/muc/ChatClient.java
@@ -62,17 +62,17 @@ public class ChatClient {
         }
     }
 
-    private void msg(String sendTo, String msgBody) throws IOException {
+    public void msg(String sendTo, String msgBody) throws IOException {
         String cmd = "msg " + sendTo + " " + msgBody + "\n";
         serverOut.write(cmd.getBytes());
     }
 
-    private void logoff() throws IOException{
+    public void logoff() throws IOException{
         String cmd = "logoff\n";
         serverOut.write(cmd.getBytes());
     }
 
-    private boolean login(String login, String password) throws IOException{
+    public boolean login(String login, String password) throws IOException{
         String cmd = "login " + login + " " + password + "\n";
         serverOut.write(cmd.getBytes());
 
@@ -151,7 +151,7 @@ public class ChatClient {
         }
     }
 
-    private boolean connect() {
+    public boolean connect() {
         try {
             this.socket = new Socket(serverName,serverPort);
             System.out.println("Client port is "+socket.getLocalPort());
---
+++ ChatClient/src/com/muc/UserListPane.java
@@ -0,0 +1,56 @@
+package com.muc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class UserListPane extends JPanel implements UserStatusListener{
+
+    private final ChatClient client;
+    private JList<String> userListUI;
+    private DefaultListModel<String> userListModel;
+
+    public UserListPane(ChatClient client) {
+        this.client = client;
+        this.client.addUserStatusListener(this);
+
+        userListModel = new DefaultListModel<>();
+        userListUI = new JList<>(userListModel);
+        setLayout(new BorderLayout());
+        add(new JScrollPane(userListUI),BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("localhost",8018);
+        
+        UserListPane userListPane = new UserListPane(client);
+        JFrame frame = new JFrame("User List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,600);
+        
+        frame.getContentPane().add(userListPane, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+        if (client.connect()) {
+            try{
+                if(client.login("guest","guest")){
+
+                } else{
+
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void online(String login) {
+        userListModel.addElement(login);
+    }
+
+    @Override
+    public void offline(String login) {
+        userListModel.removeElement(login);
+    }
+}
